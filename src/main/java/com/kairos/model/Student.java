cc211bb79bbfb9c41f11f2e55eb8ae83240d7be2
Geovanni Martins <geovanni.mrts@gmail.com>
2026-04-24 20:30:44 -0300

    ajust

--- src/main/java/com/kairos/model/Student.java
+++ src/main/java/com/kairos/model/Student.java
@@ -4,28 +4,17 @@ import java.util.ArrayList;
 
 public class Student extends User {
     private int level, xp;
-    private ArrayList<Topic> chosenTopic;
 
     public Student(String name, String email, String password, String role) {
         super(name, email, password, role);
         level = 0;
         xp = 0;
-        chosenTopic = new ArrayList<>();
     }
 
     public Student(int id, String name, String email, String password, String role, int level, int xp) {
         super(id, name, email, password, role);
         this.level = level;
         this.xp = xp;
-        chosenTopic = new ArrayList<>();
-    }
-
-    public ArrayList<Topic> getChosenTopic() {
-        return chosenTopic;
-    }
-
-    public void setChosenTopic(ArrayList<Topic> chosenTopic) {
-        this.chosenTopic = chosenTopic;
     }
 
     public int getXp() {
