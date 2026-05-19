c29a7825a6c3790d02e25278ef99e3b2d38ff8f5
davicarvalh0 <davimc0612@gmail.com>
2026-05-19 14:27:33 -0300

    adicionando o UTF-8 para rodar certinho em cada máquina

--- src/main/java/com/kairos/util/DBConnection.java
+++ src/main/java/com/kairos/util/DBConnection.java
@@ -6,7 +6,7 @@ import java.sql.SQLException;
 
 public class DBConnection {
 
-    private static final String URL = "jdbc:mysql://localhost:3307/kairosdb?allowPublicKeyRetrieval=true&useSSL=false";
+    private static final String URL = "jdbc:mysql://localhost:3307/kairosdb?allowPublicKeyRetrieval=true&useSSL=false&useUnicode=true&characterEncoding=UTF-8";
     private static final String USER = "root";
     private static final String PASSWORD = "0000";
 
